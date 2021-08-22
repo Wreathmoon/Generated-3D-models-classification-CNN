c1c49c68ed52447d249ccab0bd5ebd507d8b9bc4
Wreathmoon <88706083+Wreathmoon@users.noreply.github.com>
2021-08-22 20:46:10 +0800

    Update scad2stl.py

--- scad2stl.py
+++ scad2stl.py
@@ -1,14 +1,14 @@
 from os import listdir
 from subprocess import call
 import os
-dir = r'D:\OpenSCAD\openscad'
-files = listdir(r'C:\Users\Wreathmoon\Desktop\Dataset2\cylinder\scad')
+dir = r'path of OpenScad.exe'
+files = listdir(r'path')
 for f in files:
     if f.find(".scad") >= 0:            # get all .scad files in directory
         of = f.replace('.scad', '.stl') # name of the outfile .stl
         print(f)
-        of = os.path.join(r'C:\Users\Wreathmoon\Desktop\Dataset2\cylinder\stl', of)
-        f =  os.path.join(r'C:\Users\Wreathmoon\Desktop\Dataset2\cylinder\scad', f)
+        of = os.path.join(r'path of stl files', of)
+        f =  os.path.join(r'path of scad files', f)
         cmd = 'call (["{}",  "-o", "{}",  "{}"])'.format(dir, of, f)  # create openscad command
         os.system("call " + dir + ' -o ' + of + ' ' + f)
         print(cmd)
